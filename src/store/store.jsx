5af0c1ca86bb88fc499e993289d6bc503f5db0b1
Hikmat Rajabli <iamrajabli@outlook.com>
2022-08-15 02:35:39 +0400

    configureStore()

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,9 +1,15 @@
-import { legacy_createStore as createStore, combineReducers, applyMiddleware, compose } from "redux";
-import ReduxThunk from 'redux-thunk';
 import { products, filters } from '../reducers';
+import { configureStore } from "@reduxjs/toolkit";
 
-const store = createStore(combineReducers({ products, filters }),
-    compose(applyMiddleware(ReduxThunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+// const store = createStore(combineReducers({ products, filters }),
+//     compose(applyMiddleware(ReduxThunk),
+//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+
+
+const store = configureStore({
+    reducer: { products, filters },
+    middleware: getDefaultMiddleware => getDefaultMiddleware(),
+    devTools: process.env.NODE_ENV !== 'prodaction'
+})
 
 export default store;
