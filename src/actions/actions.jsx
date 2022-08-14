9d595367e155f53cb9c2a3db549285fbc5b8c31c
Hikmat Rajabli <iamrajabli@outlook.com>
2022-08-15 02:48:12 +0400

    createAction()

--- src/actions/actions.jsx
+++ src/actions/actions.jsx
@@ -1,3 +1,5 @@
+import { createAction } from "@reduxjs/toolkit";
+
 export const fetchData = (request, action, url = 'http://localhost:3000/products') => dispatch => {
     dispatch(productsFetching());
     request(url)
@@ -11,63 +13,79 @@ export const fetchRemovingProduct = (request, id) => dispatch => {
         .catch(_ => dispatch(productsFetchingError()))
 }
 
+export const productsFetching = createAction('PRODUCTS_FETCHING')
 
-export const productsFetching = _ => {
-    return {
-        type: 'PRODUCTS_FETCHING'
-    }
-}
+// export const productsFetching = _ => {
+//     return {
+//         type: 'PRODUCTS_FETCHING'
+//     }
+// }
 
-export const productsFetchingError = _ => {
-    return {
-        type: 'PRODUCTS_FETCHING_ERROR'
-    }
-}
+export const productsFetchingError = createAction('PRODUCTS_FETCHING_ERROR')
 
-export const productsFetched = products => {
-    return {
-        type: 'PRODUCTS_FETCHED',
-        payload: products
-    }
-}
+// export const productsFetchingError = _ => {
+//     return {
+//         type: 'PRODUCTS_FETCHING_ERROR'
+//     }
+// }
+export const productsFetched = createAction('PRODUCTS_FETCHED')
 
-export const productRemoved = id => {
-    return {
-        type: 'PRODUCT_REMOVED',
-        payload: id
-    }
-}
+// export const productsFetched = products => {
+//     return {
+//         type: 'PRODUCTS_FETCHED',
+//         payload: products
+//     }
+// }
 
-export const productMaxPriceFetched = price => {
-    return {
-        type: 'PRODUCT_MAX_PRICE_FETCHED',
-        payload: price
-    }
-}
+export const productRemoved = createAction('PRODUCT_REMOVED')
 
-export const filtersChangeRange = range => {
-    return {
-        type: 'FILTERS_CHANGE_RANGE',
-        payload: range
-    }
-}
+// export const productRemoved = id => {
+//     return {
+//         type: 'PRODUCT_REMOVED',
+//         payload: id
+//     }
+// }
 
-export const filtersChooseClothes = name => {
-    return {
-        type: 'FILTERS_CHOOSE_CLOTHES',
-        payload: name
-    }
-}
+export const productMaxPriceFetched = createAction('PRODUCT_MAX_PRICE_FETCHED')
 
-export const filtersClothesLabelsFetched = labels => {
-    return {
-        type: 'FILTERS_CLOTHES_LABELS_FETCHED',
-        payload: labels
-    }
-}
+// export const productMaxPriceFetched = price => {
+//     return {
+//         type: 'PRODUCT_MAX_PRICE_FETCHED',
+//         payload: price
+//     }
+// }
 
-export const filtersReset = _ => {
-    return {
-        type: 'FILTERS_RESET'
-    }
-}
+export const filtersChangeRange = createAction('FILTERS_CHANGE_RANGE')
+
+// export const filtersChangeRange = range => {
+//     return {
+//         type: 'FILTERS_CHANGE_RANGE',
+//         payload: range
+//     }
+// }
+
+export const filtersChooseClothes = createAction('FILTERS_CHOOSE_CLOTHES')
+
+// export const filtersChooseClothes = name => {
+//     return {
+//         type: 'FILTERS_CHOOSE_CLOTHES',
+//         payload: name
+//     }
+// }
+
+export const filtersClothesLabelsFetched = createAction('FILTERS_CLOTHES_LABELS_FETCHED')
+
+// export const filtersClothesLabelsFetched = labels => {
+//     return {
+//         type: 'FILTERS_CLOTHES_LABELS_FETCHED',
+//         payload: labels
+//     }
+// }
+
+export const filtersReset = createAction('FILTERS_RESET')
+
+// export const filtersReset = _ => {
+//     return {
+//         type: 'FILTERS_RESET'
+//     }
+// }
