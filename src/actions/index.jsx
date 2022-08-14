0fee82ec1bc5635f0ad7d1d6f52e69a093cfbb74
Hikmat Rajabli <iamrajabli@outlook.com>
2022-08-15 03:16:13 +0400

    createReducer()

--- src/actions/index.jsx
+++ src/actions/index.jsx
@@ -1,4 +1,6 @@
 export {
+    productsFetching,
+    productsFetchingError,
     productsFetched,
     productRemoved,
     productMaxPriceFetched,
--- src/reducers/products.jsx
+++ src/reducers/products.jsx
@@ -1,47 +1,94 @@
+import { createReducer } from "@reduxjs/toolkit";
+import {
+    productsFetching,
+    productsFetchingError,
+    productsFetched,
+    productRemoved,
+    productMaxPriceFetched
+} from '../actions';
+
 const initialState = {
     products: [],
     loading: 'idle',
     maxPrice: 0,
 }
 
-const products = (state = initialState, action) => {
-    switch (action.type) {
+const products = createReducer(initialState, {
+    [productsFetching]: state => { state.loading = 'loading' },
+    [productsFetchingError]: state => {
+        state.loading = 'error';
+        state.products = [];
+    },
+    [productsFetched]: (state, action) => {
+        state.loading = 'idle';
+        state.products = action.payload;
+    },
+    [productRemoved]: (state, action) => { state.products = state.products.filter(product => product.id !== action.payload) },
+    [productMaxPriceFetched]: (state, action) => { state.maxPrice = action.payload.sort((b, a) => b.price - a.price)[action.payload.length - 1].price }
+},
+    [],
+    state => state
+)
 
-        case 'PRODUCTS_FETCHING':
-            return {
-                ...state,
-                loading: 'loading'
-            }
+// const products = createReducer(initialState, builder => {
+//     builder
+//         .addCase(productsFetching, state => {
+//             state.loading = 'loading'
+//         })
+//         .addCase(productsFetchingError, state => {
+//             state.loading = 'error';
+//             state.products = [];
+//         })
+//         .addCase(productsFetched, (state, action) => {
+//             state.loading = 'idle';
+//             state.products = action.payload;
+//         })
+//         .addCase(productRemoved, (state, action) => {
+//             state.products = state.products.filter(product => product.id !== action.payload)
+//         })
+//         .addCase(productMaxPriceFetched, (state, action) => {
+//             state.maxPrice = action.payload.sort((b, a) => b.price - a.price)[action.payload.length - 1].price
+//         })
+// })
 
-        case 'PRODUCTS_FETCHING_ERROR':
-            return {
-                ...state,
-                loading: 'error',
-                products: []
-            }
+// const products = (state = initialState, action) => {
+//     switch (action.type) {
 
-        case 'PRODUCTS_FETCHED':
-            return {
-                ...state,
-                loading: 'idle',
-                products: action.payload,
-            }
+//         case 'PRODUCTS_FETCHING':
+//             return {
+//                 ...state,
+//                 loading: 'loading'
+//             }
 
-        case 'PRODUCT_REMOVED':
-            return {
-                ...state,
-                products: state.products.filter(product => product.id !== action.payload)
-            }
+//         case 'PRODUCTS_FETCHING_ERROR':
+//             return {
+//                 ...state,
+//                 loading: 'error',
+//                 products: []
+//             }
 
-        case 'PRODUCT_MAX_PRICE_FETCHED':
-            return {
-                ...state,
-                maxPrice: action.payload.sort((b, a) => b.price - a.price)[action.payload.length - 1].price
-            }
+//         case 'PRODUCTS_FETCHED':
+//             return {
+//                 ...state,
+//                 loading: 'idle',
+//                 products: action.payload,
+//             }
 
-        default:
-            return state;
-    }
-}
+//         case 'PRODUCT_REMOVED':
+//             return {
+//                 ...state,
+//                 products: state.products.filter(product => product.id !== action.payload)
+//             }
+
+//         case 'PRODUCT_MAX_PRICE_FETCHED':
+//             return {
+//                 ...state,
+//                 maxPrice: action.payload.sort((b, a) => b.price - a.price)[action.payload.length - 1].price
+//             }
+
+//         default:
+//             return state;
+//     }
+// }
 
 export default products;
